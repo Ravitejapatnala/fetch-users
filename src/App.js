478a3410d22ee8e64ffa55a4d433f98b56fa8f8d
akhil <akhil.ce08@gmail.com>
2024-01-19 15:53:32 +0530

    first commit and uploaded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const[data, setData]= useState([]);
+
+  function getUsers(){
+    fetch("https://reqres.in/api/users")
+    .then((response)=>response.json())
+    .then((user)=>setData(user.data))
+    .catch((error)=>console.log(error))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h3>Blue Whales</h3>
+        <button onClick={getUsers}>Users List</button>
+      </div>
+      {
+        data && (
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {
+                  data.map((item)=>(
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.email}</td>
+                      <td>{item.first_name}</td>
+                      <td>{item.last_name}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default App;
